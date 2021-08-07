a972d498bdde63fe70f38ef2b133fbdc6895cfc3
joyatee-datta-2020 <joyetaratri123@gmail.com>
2021-08-08 02:09:33 +0600

    codechef

---
+++ Codechef/prb52.py
@@ -0,0 +1,28 @@
+"""
+AB??ED
+AB?ED?
+
+"""
+T = int(input())
+for i in range():
+    X = input()
+    Y = input()
+
+    match = True
+
+
+    for j in range(len(Y)):
+        if X[j] == "?" or Y[j]=="?" or X[j] == Y[j]:
+            continue
+        else:
+            match = False
+            break
+
+    if match:
+        print("Yes")
+    else:
+        print("No")
+
+
+
+
