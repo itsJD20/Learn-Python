27f69ec3c89cd9a38e29765a35175e74fc7154ef
joyatee-datta-2020 <joyetaratri123@gmail.com>
2021-08-08 01:49:10 +0600

    codechef

--- Codechef/prb51.py
+++ Codechef/prb51.py
@@ -1,6 +1,11 @@
 """
 n = 5
 A = 2 5 3 7 8 
+2 3 7 8                 cost - 2
+2 7 8                   cost - 2 + 2
+2 8                     cost - 2 + 2 + 2
+2                       cost - 2 + 2 + 2 + 2
+
 2
 A = 2 3 7 8
 4
@@ -9,5 +14,30 @@ A = 2 3 7 8
 2 8
 8
 
-(n-1)* min(A)
+(n-1) * min(A)
 """
+T = int(input())
+for j in range(T):
+    n= int(input())
+    A = list(map(int,input().split()))
+    l =A[0]
+
+    for i in range(len(A)):
+        if A[i] < l:
+            l = A[i]
+
+
+        
+    print((n-1)*l)
+
+
+
+
+
+
+
+
+
+
+
+
