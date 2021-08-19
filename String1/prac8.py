21b254123c07360e1b9a2d78ecad1c27f0bdfcac
joyatee-datta-2020 <joyetaratri123@gmail.com>
2021-08-20 01:53:19 +0600

    pattern

--- String1/prac8.py
+++ String1/prac8.py
@@ -1,29 +1,20 @@
 """
-st1 = "_-__-_-__--___-_"
-                       |     
-st2 = "-__-"
-          |  
-Ans =2
+st1 = "__--_-__-"
+            |     
+st2 = "__-"
+       |  
+Ans = 2
 
 """
-st1 = input()
-st2 = input()
-ptr1 = 0 #14
+
+str1 = input()
+str2 = input()
+l = len(str2)
 count = 0
-for i in range(len(st1)): #4
-    
-    ptr2 = 0 #
-    while ptr1 < len(st1) and ptr2 < len(st2):
-        if st1[ptr1] == st2[ptr2]:
-            ptr1 += 1
-            ptr2 += 1
-        else:
-            ptr1 += 1
-
-    count += 1 #3
-
+for i in range(len(str1)):
+    if str2 == str1[i:l+i]:
+        count += 1
 print(count)
-        
 
 
 
