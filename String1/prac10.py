c42a6b0f1b53531db250ac288e85c782b2988d72
joyatee-datta-2020 <joyetaratri123@gmail.com>
2021-08-21 00:09:21 +0600

    B to D

--- String1/prac10.py
+++ String1/prac10.py
@@ -1,11 +1,25 @@
 """
 st = "1010"
+        |
+    0*2^0 + 1*2^1 + 0*2^2 + 1*2^3    
 Ans = 10
 
 st = "111"
 Ans = 7
 
 """
+st = input() #10101
+
+pt = len(st) - 1 
+s = 0 #
+for i in range(len(st)): #
+    
+    k = int(st[pt]) *(2**i)
+    s += k
 
 
+    pt -= 1
+
+print(s)
+        
 
