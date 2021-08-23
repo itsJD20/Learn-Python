dff374c576ca83724119eed8888b8220875624d8
joyatee-datta-2020 <joyetaratri123@gmail.com>
2021-08-24 01:30:45 +0600

    string

--- String1/prac12.py
+++ String1/prac12.py
@@ -6,4 +6,36 @@ Ans = True
 st1 = "kwkwqwe"
 st2 = "abcdefg"
 Ans = False
+
+1. st1[i] not in d
+2. st1[i] in d and st2[i] == d[st1[i]]
+3. st1[i] in d and st2[i] != d[st1[i]]
 """
+
+st1 = input()
+st2 = input()
+
+match = True
+
+d = {}
+
+for i in range(len(st1)):
+
+    
+    if st1[i] not in d:
+        d[st1[i]] = st2[i]
+    elif st1[i] in d and st2[i] == d[st1[i]]:
+        continue
+    else:
+        match = False
+        break
+
+if match:
+    print(True)
+else:
+    print(False)
+
+
+
+
+
---
+++ class/4/Ex1.py
@@ -0,0 +1,10 @@
+class Product():
+    def __init__(self, id, name, catagory):
+        self.id = id
+        self.name = name
+        self.catagory = catagory
+        self.price = 1000
+
+    def price
+
+
