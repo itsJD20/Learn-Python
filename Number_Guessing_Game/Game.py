72da8fe905657122646f5c6b1f3e8ad1a14433e1
joyatee-datta-2020 <joyetaratri123@gmail.com>
2021-09-12 01:26:06 +0600

    Number Game

---
+++ Number_Guessing_Game/Game.py
@@ -0,0 +1,43 @@
+import random
+
+def rand(l,u):
+
+    return int(l + random.random() * (u-l))
+
+
+a = 1
+b = 1000
+
+r = rand(a,b)
+f =input("Enter the name of 1st Player: ")
+s =input("Enter the name of 2nd Player: ")
+
+attempts = 50
+p_num = 1
+while True:
+    if p_num == 1:
+        player = f
+        p_num = 2
+    else:
+        player = s
+        p_num = 1
+    user = int(input(player + ": "))
+    if user == r:
+        print(player + " WIN ")
+        break
+    elif user > r:
+        print("GIVE A SMALLER NUMBER")
+    else:
+        print("GIVE A BIGGER NUMBER")
+
+    attempts -= 1
+
+
+
+
+
+    
+
+
+
+
