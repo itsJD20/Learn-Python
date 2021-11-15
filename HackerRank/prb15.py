6a460d3c2a897d9849c5d220f0be6af43a9496d2
joyatee-datta-2020 <joyetaratri123@gmail.com>
2021-11-16 00:44:13 +0600

    new

--- HackerRank/prb15.py
+++ HackerRank/prb15.py
@@ -1,18 +1,26 @@
 n = int(input())
 s = list(map(int,input().split()))#1,2,1,2,1,3,2
 
+d ={}
+
+for i in range(len(s)):#2
+    if s[i] in d:
+        d[s[i]] += 1
+    else:
+        d[s[i]] = 1
+
+pair =0#2
+for i in d:#3
+    if d[i]>= 2:
+        cal = d[i]//2
+        pair += cal
+
+print(pair)
 
 
-for i in range(len(s)):#0
-    count =0
-    l =[]
-    for j in range(len(s)):#0
-        if s[i]== s[j]:
-            count += 1
-            if s[i] not in l:
-                l.append(s[i])
 
-        
+    
+
             
     
 
